06322f38c6e9812fe9ab9a15294bafb8f7d1edba
GUXI\guxi <guxi@live.com>
2019-08-15 10:38:45 +0800

    Basic view now return html

--- functional_test.py
+++ functional_test.py
@@ -5,14 +5,14 @@ import unittest
 class NewVisitor(unittest.TestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(3)
     
     def tearDown(self):
         self.browser.quit()
         
     def test_can_start_a_list_and_retrieve_it_laster(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title)
+        self.assertIn('to-do',self.browser.title)
         self.fail('finish the test!')
         
 if __name__=='__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 
 # =============================================================================
@@ -13,4 +14,14 @@ class HomePageTest(TestCase):
     def test_root_url_resolvers_to_homepage_view(self):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
+        
+    def test_hompage_return_correct_html(self):
+        request=HttpRequest()
+        response=home_page(request)
+        a=response.content
+        print(a)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>to-do</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+        
         
