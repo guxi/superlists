cbc1e0833db3b5655c74f7502ef47043f523585e
GUXI\guxi <guxi@live.com>
2019-08-15 11:07:04 +0800

    functional test now add to_do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitor(unittest.TestCase):
@@ -12,7 +13,24 @@ class NewVisitor(unittest.TestCase):
         
     def test_can_start_a_list_and_retrieve_it_laster(self):
         self.browser.get('http://localhost:8000')
+        
         self.assertIn('to-do',self.browser.title)
+        head_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('to-do',head_text)
+        
+        inputbox=self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+                inputbox.get_attribute("placeholder"),
+                'Enter a to_do item')
+        inputbox.send_keys('buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(rows.text=='1: Buy peacock feathers' for row in rows)
+                )
+         
         self.fail('finish the test!')
         
 if __name__=='__main__':
